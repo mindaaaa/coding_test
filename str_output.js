2e81779801c90efff5fbddc9119382a8286bbc24
mindaaaa <devalc735@gmail.com>
2024-05-31 17:35:03 +0900

    문자열 90도로 출력하기

---
+++ str_output.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (line) {
+  line.split('').map((str) => console.log(str));
+});
